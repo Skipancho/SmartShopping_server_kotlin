599697f6dfd23f6c2973cbee704bf736c3961e3d
Skipancho <ctg1770a@gmail.com>
2021-12-22 11:24:32 +0900

    + 기본 세팅

---
+++ src/main/kotlin/com/example/smartshopping/common/SmartShoppingException.kt
@@ -0,0 +1,3 @@
+package com.example.smartshopping.common
+
+class SmartShoppingException(message : String) : RuntimeException(message)
---
+++ src/main/kotlin/com/example/smartshopping/controller/TestController.kt
@@ -0,0 +1,11 @@
+package com.example.smartshopping.controller
+
+import com.example.smartshopping.common.ApiResponse
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestController {
+    @GetMapping("/api/v1/hello")
+    fun hello() = ApiResponse.ok("world")
+}
