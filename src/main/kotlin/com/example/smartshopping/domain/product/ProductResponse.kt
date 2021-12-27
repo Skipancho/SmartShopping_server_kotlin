536a5c75adf821dcb72f201ef57f046772eebe80
Skipancho <ctg1770a@gmail.com>
2021-12-27 14:11:57 +0900

    + 상품 검색 로직 작성

---
+++ src/main/kotlin/com/example/smartshopping/domain/product/ProductResponse.kt
@@ -0,0 +1,20 @@
+package com.example.smartshopping.domain.product
+
+import com.example.smartshopping.common.SmartShoppingException
+
+data class ProductResponse(
+    val id : Long,
+    val name :String,
+    val description : String,
+    val price : Int,
+    val status : String,
+    val sellerId : Long,
+    val imagePaths : List<String>
+)
+
+fun Product.toProductResponse() = no?.let {
+    ProductResponse(
+        it, name, description, price, status.name, userCode,
+        images.map { it.path }
+    )
+} ?: throw SmartShoppingException("상품 정보를 찾을 수 없습니다.")
