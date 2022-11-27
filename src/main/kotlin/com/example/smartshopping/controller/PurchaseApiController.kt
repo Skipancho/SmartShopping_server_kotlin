a27a3106dac50e282d06f3387df3ecd4956b1d93
skipancho <ctg1770a@gmail.com>
2022-11-27 15:16:39 +0900

    [FEAT] - 구매 목록 등록 함수 추가 (Service, ApiController)

--- src/main/kotlin/com/example/smartshopping/controller/PurchaseApiController.kt
+++ src/main/kotlin/com/example/smartshopping/controller/PurchaseApiController.kt
@@ -2,24 +2,29 @@ package com.example.smartshopping.controller
 
 import com.example.smartshopping.common.ApiResponse
 import com.example.smartshopping.domain.purchase.PurchaseRecord
+import com.example.smartshopping.domain.purchase.PurchaseRequest
 import com.example.smartshopping.domain.purchase.PurchaseService
 import com.example.smartshopping.domain.purchase.toPurchaseResponse
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1")
 class PurchaseApiController @Autowired constructor(
     private val purchaseService: PurchaseService
-){
+) {
     @GetMapping("/purchase")
     fun getPurchaseRecord(
-        @RequestParam year : Int,
-        @RequestParam month : Int
+        @RequestParam year: Int,
+        @RequestParam month: Int
     ) = purchaseService.getPurchaseRecord(year, month)
-        .map(PurchaseRecord :: toPurchaseResponse)
+        .map(PurchaseRecord::toPurchaseResponse)
         .let { ApiResponse.ok(it) }
+
+    @PostMapping("/purchase")
+    fun register(
+        @RequestBody request: PurchaseRequest
+    ) = ApiResponse.ok(
+        purchaseService.register(request)
+    )
 }
--- src/main/kotlin/com/example/smartshopping/domain/purchase/PurchaseService.kt
+++ src/main/kotlin/com/example/smartshopping/domain/purchase/PurchaseService.kt
@@ -4,6 +4,7 @@ import com.example.smartshopping.common.SmartShoppingException
 import com.example.smartshopping.domain.auth.UserContextHolder
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class PurchaseService @Autowired constructor(
@@ -15,4 +16,14 @@ class PurchaseService @Autowired constructor(
             purchaseRepository.findByUserCodeAndYearAndMonthOrderByIdDesc(it, year, month)
         } ?: throw SmartShoppingException("구매 기록 조회에 필요한 사용자 정보가 존재하지 않습니다.")
     }
+
+    fun register(purchaseRequest: PurchaseRequest) {
+        userContextHolder.userCode?.let {
+            val calendar = Calendar.getInstance()
+            purchaseRequest.toPurchaseRecord(it, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH) + 1)
+                .run(::save)
+        } ?: throw SmartShoppingException("구매 기록 등록에 필요한 사용자 정보가 존재하지 않습니다.")
+    }
+
+    private fun save(purchaseRecord: PurchaseRecord) = purchaseRepository.save(purchaseRecord)
 }
